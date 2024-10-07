2bc9702f2c9220030641e1d19700a68f34b4dcc6
Connor <concon477@virginmedia.com>
2024-10-07 20:13:01 +0100

    fix: Game id is not an incrementing value

--- Engine/src/main/kotlin/org/readutf/game/engine/GameManager.kt
+++ Engine/src/main/kotlin/org/readutf/game/engine/GameManager.kt
@@ -1,14 +1,17 @@
 package org.readutf.game.engine
 
 import java.util.UUID
+import java.util.concurrent.atomic.AtomicInteger
 
 object GameManager {
-    val activeGames = mutableListOf<Game<*>>()
+    val activeGames = mutableMapOf<String, Game<*>>()
     val playerToGame = mutableMapOf<UUID, Game<*>>()
 
     fun getGameByPlayer(playerId: UUID): Game<*>? = playerToGame[playerId]
 
-    fun getGameById(gameId: String): Game<*>? =
-        activeGames
-            .firstOrNull { game -> game.gameId.toString().startsWith(gameId) }
+    fun getGameById(gameId: String): Game<*>? = activeGames[gameId]
+
+    val tracker = AtomicInteger(1000)
+
+    fun generateGameId(): String = tracker.incrementAndGet().toString()
 }

2cd661386139aab872ab34f21d8798993676c7f9
Connor <concon477@virginmedia.com>
2024-10-07 20:12:09 +0100

    fix: Respawn event is mutable, and only called when result is known

--- Engine/src/main/kotlin/org/readutf/game/engine/event/impl/GameRespawnEvent.kt
+++ Engine/src/main/kotlin/org/readutf/game/engine/event/impl/GameRespawnEvent.kt
@@ -4,10 +4,9 @@ import net.minestom.server.entity.Player
 import org.readutf.game.engine.Game
 import org.readutf.game.engine.event.GameEvent
 import org.readutf.game.engine.respawning.RespawnPosition
-import org.readutf.game.engine.types.Result
 
 class GameRespawnEvent(
     game: Game<*>,
     val player: Player,
-    val respawnPositionResult: Result<RespawnPosition>,
+    var respawnPositionResult: RespawnPosition,
 ) : GameEvent(game)

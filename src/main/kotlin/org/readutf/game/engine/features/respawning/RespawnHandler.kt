3e45fab9eb9e1187be64a9c971d55cfe035cc9cb
utfunderscore <83981186+utfunderscore@users.noreply.github.com>
2025-02-05 00:25:31 +0000

    fix: Add player id to RespawnHandler

--- src/main/kotlin/org/readutf/game/engine/features/respawning/RespawnHandler.kt
+++ src/main/kotlin/org/readutf/game/engine/features/respawning/RespawnHandler.kt
@@ -1,8 +1,9 @@
 package org.readutf.game.engine.features.respawning
 
 import org.readutf.game.engine.utils.Position
+import java.util.UUID
 
 fun interface RespawnHandler {
 
-    fun findRespawnLocation(): Position
+    fun findRespawnLocation(playerId: UUID): Position
 }

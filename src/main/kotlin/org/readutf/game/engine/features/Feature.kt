36d3aa0f8e21720e846e525decd6407a9c06fcfb
utfunderscore <83981186+utfunderscore@users.noreply.github.com>
2025-02-10 23:32:49 +0000

    fix: Apply KtLint formatting

--- src/main/kotlin/org/readutf/game/engine/features/Feature.kt
+++ src/main/kotlin/org/readutf/game/engine/features/Feature.kt
@@ -2,7 +2,6 @@ package org.readutf.game.engine.features
 
 import org.readutf.game.engine.event.listener.GameListener
 import org.readutf.game.engine.schedular.GameTask
-import org.readutf.game.engine.schedular.RepeatingGameTask
 import kotlin.reflect.KClass
 
 abstract class Feature {
